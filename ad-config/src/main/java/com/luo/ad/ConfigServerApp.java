b05a42aa10c2ca6ca7069fe3d1de6aa62fd9416a
Administrator <2084267015@qq.com>
2019-12-14 20:15:48 +0800

    st

--- ad-config/src/main/java/com/luo/ad/ConfigServerApp.java
+++ ad-config/src/main/java/com/luo/ad/ConfigServerApp.java
@@ -2,8 +2,8 @@ package com.luo.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 1.git init
@@ -18,9 +18,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
-public class ConfigApp {
+@EnableDiscoveryClient
+public class ConfigServerApp {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApp.class, args);
+        SpringApplication.run(ConfigServerApp.class, args);
     }
 }
--- ad-eureka-client/src/main/java/com/luo/ad/web/controller/StatusCtr.java
+++ ad-eureka-client/src/main/java/com/luo/ad/web/controller/StatusCtr.java
@@ -1,13 +1,11 @@
 package com.luo.ad.web.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusCtr {
-    @Value("${sf.user.id}")
-    private int id;
+
 
     @GetMapping("status")
     public String status() {
