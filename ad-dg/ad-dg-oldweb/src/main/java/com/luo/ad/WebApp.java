2cb3adf9c50d947d2402021b01caa227c839c068
Administrator <2084267015@qq.com>
2019-12-15 20:15:41 +0800

    st

--- ad-dg/ad-dg-oldweb/src/main/java/com/luo/ad/WebApp.java
+++ ad-dg/ad-dg-oldweb/src/main/java/com/luo/ad/WebApp.java
@@ -2,11 +2,12 @@ package com.luo.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-//@EnableHystrix
-@EnableCircuitBreaker
+@EnableHystrix
+//or use
+//@EnableCircuitBreaker
 public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
