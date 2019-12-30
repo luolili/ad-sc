5c2a71381232321d2ae73227da27600df5917876
Administrator <2084267015@qq.com>
2019-12-30 11:35:27 +0800

    st

---
+++ ad-user-api/src/main/java/com/luo/ad/domain/Role.java
@@ -0,0 +1,8 @@
+package com.luo.ad.domain;
+
+import javax.validation.constraints.NotBlank;
+
+public class Role {
+    @NotBlank
+    private String name;
+}
--- ad-user-api/src/main/java/com/luo/ad/domain/User.java
+++ ad-user-api/src/main/java/com/luo/ad/domain/User.java
@@ -2,9 +2,15 @@ package com.luo.ad.domain;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @Data
 public class User {
 
     private Long id;
     private String name;
+
+    //级联valid
+    private List<@Valid Role> roleList;
 }
